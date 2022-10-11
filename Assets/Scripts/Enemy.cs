753bdab198ece40f7e74bfcb88e30532618a7653
beluvinse <83471976+beluvinse@users.noreply.github.com>
2022-10-11 20:16:59 -0300

    aver si anda xd

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Values")]
+    [SerializeField] private float _life;
+    [SerializeField] private float _maxLife;
+    [SerializeField] private float _damage;
+    [SerializeField] private float _speed;
+
+}
