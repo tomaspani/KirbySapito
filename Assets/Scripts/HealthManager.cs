5af142484b8ec8ce45ec24770d9326670a29ad33
beluvinse <83471976+beluvinse@users.noreply.github.com>
2022-10-14 16:20:11 -0300

    spikes !
    commiteo por las dudas

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class HealthManager : MonoBehaviour
 {
@@ -8,9 +10,29 @@ public class HealthManager : MonoBehaviour
     [SerializeField] private float _maxHealth;
     [SerializeField] private float _health;
 
+    public Image myBar;
+
+    internal void Initalize()
+    {
+        _health = _maxHealth;
+    }
+
     void TakeDamage()
     {
 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Spikes spike = other.gameObject.GetComponent<Spikes>();
+
+        if (spike)
+        {
+            //spike._damage --> como entro si es privado??
+            _health -= 5;
+        }
+
+        myBar.fillAmount = _health / _maxHealth;
     }
 
 }
---
+++ Assets/Scripts/Spikes.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+
+    private float _damage = 10;
+
+    void Start()
+    {
+        StartCoroutine(Go());
+    }
+    
+    IEnumerator Go()
+    {
+        while(true)
+        {
+            var number = Random.Range(3, 8);
+            GetComponent<Animation>().Play();
+            yield return new WaitForSeconds(number);
+        }
+    }
+
+}
