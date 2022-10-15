7f5a2cd7cb7e1c194f68219a08af1148132d6d95
tomaspani <tomas.paniego@davinci.edu.ar>
2022-10-14 22:23:57 -0300

    Commit entrega

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -6,6 +6,7 @@ public class BulletController : MonoBehaviour
 {
     [SerializeField] private float _speed;
     [SerializeField] private float _damage;
+    [SerializeField] private string hitTag;
 
 
     private void Update()
@@ -15,12 +16,48 @@ public class BulletController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        
-        if(other.gameObject.tag == "Enemy")
+
+        if (other.gameObject.tag == hitTag)
         {
-            other.GetComponent<Enemy>().TakeDamage(_damage);
-            Debug.Log("mepegaste ");
+            if (hitTag == "Enemy")
+            {
+                other.GetComponent<Enemy>().TakeDamage(_damage);
+                Debug.Log("mepegaste ");
+            }
+            else if (hitTag == "Player")
+            {
+                other.GetComponent<HealthManager>().TakeDamage(_damage);
+                Debug.Log("mepegaste jugador ");
+            }
+            Destroy(this.gameObject);
+
         }
-        Destroy(this.gameObject);
+        if (other.gameObject.tag == "Wall")
+        {
+            Destroy(this.gameObject);
+
+        }
     }
+
+    /*private void OnTriggerStay(Collider other)
+    {
+
+        if (other.gameObject.tag == hitTag)
+        {
+            if (hitTag == "Enemy")
+            {
+                other.GetComponent<Enemy>().TakeDamage(_damage);
+                Debug.Log("mepegaste ");
+            }
+            else if (hitTag == "Player")
+            {
+                other.GetComponent<HealthManager>().TakeDamage(_damage);
+                Debug.Log("mepegaste jugador ");
+            }
+            Destroy(this.gameObject);
+        }
+
+       
+
+    }*/
 }
--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -4,11 +4,53 @@ using UnityEngine;
 
 public class Manager : MonoBehaviour
 {
-    [SerializeField] private float _enemysToKillInWave;
+    [SerializeField] private int _enemysToKillInWave;
     [SerializeField] private int _waves;
 
+    public Transform door;
+
     private int _currentWave = 1;
-    private float _currentEnemies;
+    [SerializeField] private int currentEnemies;
+
+    
+
+    private void Start()
+    {
+        currentEnemies = _enemysToKillInWave;
+    }
+
+    private void Update()
+    {
+        if (currentEnemies == 0)
+        {
+            if(_currentWave == _waves)
+            {
+                OpenDoor();
+            }
+            else
+            {
+                _currentWave++;
+                currentEnemies = _enemysToKillInWave;
+            }
+            
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("nueva zona");
+            GetComponent<BoxCollider>().enabled = false;
+            currentEnemies = _enemysToKillInWave;
+            _currentWave = 1;
+        }
+    }
+
+    public void EnemyDown()
+    {
+        currentEnemies--;
+    }
 
 
     public int getCurrentWave()
@@ -21,25 +63,9 @@ public class Manager : MonoBehaviour
         return _waves;
     }
 
-    private void Start()
+    private void OpenDoor()
     {
-        _currentEnemies = _enemysToKillInWave;
-    }
-
-    private void Update()
-    {
-        if (_currentEnemies == 0)
-        {
-            if(_currentWave == _waves)
-            {
-                //Abrir puerta
-            }
-            else
-            {
-                _currentWave++;
-                _currentEnemies = _enemysToKillInWave;
-            }
-            
-        }
+        if (door.transform.position.x < 8f)
+            door.transform.position += Vector3.right * 5f * Time.deltaTime;
     }
 }
